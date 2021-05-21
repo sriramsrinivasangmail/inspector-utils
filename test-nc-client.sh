b78d7e27423274c74157103c419ac60b1021df64
Sriram Srinivasan <sriram@us.ibm.com>
2021-05-20 20:49:33 -0700

    find ip address of server via docker inspect

--- test-nc-client.sh
+++ test-nc-client.sh
@@ -4,7 +4,10 @@ arch=`uname -s`
 if [ X"$arch" = X"Darwin" ];
 then
   hn="--network=host"
+  svr_ip=127.0.0.1
+else
+  svr_ip=$(docker inspect --format '{{ .NetworkSettings.IPAddress }}' test-nc-server)
 fi
 #docker rm -f test-nc-client
-docker run ${hn} -i --rm quay.io/sriramsrinivasan/inspector-utils:v1 -vz 127.0.0.1 33001
+docker run ${hn} -i --rm quay.io/sriramsrinivasan/inspector-utils:v1 -vz ${svr_ip} 33001
 
--- test-nc-server.sh
+++ test-nc-server.sh
@@ -7,4 +7,4 @@ then
 fi
 
 docker rm -f test-nc-server
-docker run -idt ${hn} --name test-nc-server quay.io/sriramsrinivasan/inspector-utils:v1 -vkl 33001
+docker run -idt ${hn} -p 33001:33001 --name test-nc-server quay.io/sriramsrinivasan/inspector-utils:v1 -vkl 33001
