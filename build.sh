072f1298dbb95003881c2b7933d5df2f66deac14
Sriram Srinivasan <sriramsrinivasan@gmail.com>
2024-01-20 19:48:34 -0800

    tag image reg location separately

--- build.sh
+++ build.sh
@@ -4,6 +4,6 @@ scriptdir=`dirname $0`
 
 docker_file_name=${scriptdir}/Dockerfile
 
-docker_image_name=quay.io/sriramsrinivasan/inspector-utils:v1
-
+#docker_image_name=quay.io/sriramsrinivasan/inspector-utils:v1
+docker_image_name=inspector-utils:v1
 docker build -f ${docker_file_name} -t ${docker_image_name} .
